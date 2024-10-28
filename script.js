68cb31c3ec9edc123695d6655c5d756fe5fbcff9
faazdy <leorengifor@gmail.com>
2024-10-28 18:57:17 -0500

    -m 'jhaskjdhas'

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e)=>{
+    const user = document.querySelector('#user-input')
+    const pass = document.querySelector('#pass')
+
+    if(user.value !== 'aleonmo67650' && pass.value !== 'saxhe3-sijCuv-wuhmeh'){
+        e.preventDefault();
+        const err = document.querySelector('#error')
+        user.style.borderColor = 'red'
+        pass.style.borderColor = 'red'
+        error.style.display = 'block'
+    } else{
+        console.log('exito')
+    }
+})
