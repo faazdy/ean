49dd57becb0f040394290f055fd9182e062368c2
faazdy <leorengifor@gmail.com>
2024-11-13 16:43:06 -0500

    -m 'sadsadsadsasadsadsadsadsad'

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('form')
-
+const validUsers = ['aleonmo67650', 'geral12']
+const validPass = ['saxhe3-sijCuv-wuhmeh', 'geral12']
 form.addEventListener('submit', (e)=>{
     const user = document.querySelector('#user-input')
     const pass = document.querySelector('#pass')
 
-    if(user.value !== 'aleonmo67650' || pass.value !== 'saxhe3-sijCuv-wuhmeh'){
+    if(!validUsers.includes(user.value) || !validPass.includes(pass.value)){
         e.preventDefault();
         const err = document.querySelector('#error')
         user.style.borderColor = 'red'
